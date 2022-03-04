9da92469acde2430dad8fd508dc6f9f450058a9d
chocoh-pony <99412586+chocoh-pony@users.noreply.github.com>
2022-03-04 08:17:16 -0300

    at a0001

--- gmail.php
+++ gmail.php
@@ -7,10 +7,12 @@ if(isset($_POST['email']) && !empty($_POST['email'])){
 
   $to = "ponychocoh@gmail.com";
   $subject = "contato - programador BR";
-  $body = "Nome: ".$nome. "\r\n";
-          "email: " .$email. "\r\n";
+  $body = "Nome: ".$nome. "\r\n".
+          "email: " .$email. "\r\n".
           "mensagem: ".$mensagem;
-  $header = "forum:tuikugames@gmail.com"."\r\n"."Reply-to:".$email."\r\n"."X=Mailer:PHP/".phpversion();
+  $header = "forum:tuikugames@gmail.com"."\r\n".
+         "Reply-to:".$email."\r\n".
+         "X=Mailer:PHP/".phpversion();
   if(mail($to, $subject, $body, $header)){
       echo("email enciado com sucesso!");
   }
